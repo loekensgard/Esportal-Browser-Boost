cddd6611911b23f197bef3e0f2f55eab8fd8cffc
Thorstein Løkensgard <loekensgard@gmail.com>
2022-09-01 19:35:51 +0200

    fix: parent element was null

--- removeOnClick.js
+++ removeOnClick.js
@@ -1,5 +1,10 @@
-var elementToRemove = document.getElementById("right-side-info-content").querySelector(".live-streams");
-
+//try to remove the parent element
+var elementToRemove = document.getElementById("right-side-info-content").querySelector(".live-streamz");
 if(elementToRemove !== null){
     elementToRemove.remove();
+}
+
+//if the parent element is null, find all iframes and hide them.
+for (let i = 0; i < frames.length; i++) {
+    frames[i].style.display = "none"
 }

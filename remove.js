77376c47a1bb6c9376ba8821060fe19de5a58d80
Thorstein Løkensgard <loekensgard@gmail.com>
2022-10-17 22:42:53 +0200

    feat: add new element check for the new esportal site

--- remove.js
+++ remove.js
@@ -1,14 +1,14 @@
 window.setTimeout(() => {
-    //try to remove the parent element
-    var elementToRemove = document.getElementById("right-side-info-content").querySelector(".live-streamz");
-    if(elementToRemove !== null){
-        elementToRemove.remove();
-    }
-    
-    //if the parent element is null, find all iframes and hide them.
-    var frames = document.getElementsByTagName('iframe');
-    for (let i = 0; i < frames.length; i++) {
-        frames[i].style.display = "none"
+    //find the bottom drawer at the left side (the one with all the livestreams)
+    var sideMenu = document.querySelectorAll('[class*="PrivateLeftSideBarstyle"]');
+    if(sideMenu.length > 0){
+        var lastElement = sideMenu[sideMenu.length -1];
+        lastElement.remove();
     }
 
+    //find the popoup video player
+    var popup = document.querySelectorAll('[class*="VideoPopupstyle"]');
+    if(popup.length > 0){
+        popup.forEach(x => x.remove());
+    }
 }, 5000)

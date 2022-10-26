3ad16f141b7c9aec5c0276d7279c7bfb7d283e9b
Thorstein Løkensgard <loekensgard@gmail.com>
2022-10-26 21:04:35 +0200

    fix: add search for live-stream-section

--- remove.js
+++ remove.js
@@ -1,14 +1,36 @@
 window.setTimeout(() => {
+    var matches = []; 
+
     //find the bottom drawer at the left side (the one with all the livestreams)
     var sideMenu = document.querySelectorAll('[class*="PrivateLeftSideBarstyle"]');
     if(sideMenu.length > 0){
         var lastElement = sideMenu[sideMenu.length -1];
+    
+        matches.push(lastElement);
+    
         lastElement.remove();
     }
-
+    
     //find the popoup video player
     var popup = document.querySelectorAll('[class*="VideoPopupstyle"]');
-    if(popup.length > 0){
-        popup.forEach(x => x.remove());
+    for(var pop of popup){
+        matches.push(pop);
+        pop.remove();
     }
+    
+    //find the live-stream-section
+    var streams = document.querySelectorAll('[class*="live-steam-section"]');
+    for(var pop of streams){
+        matches.push(pop);
+        pop.remove();
+    }
+    
+    //find all iframes
+    var iframes = document.querySelectorAll('iframe');
+    for(var pop of iframes){
+        matches.push(pop);
+        pop.remove();
+    }
+    
+    console.log(matches);
 }, 5000)

dc347bf924c155c442b65322a99edebd78ff551e
Thorstein Løkensgard <loekensgard@gmail.com>
2022-10-27 12:21:45 +0200

    fix: set style.display to none instead of removing the node

--- removeOnClick.js
+++ removeOnClick.js
@@ -1,40 +1,19 @@
-var matches = []; 
-
-//find the bottom drawer at the left side (the one with all the livestreams)
-var sideMenu = document.querySelectorAll('[class*="PrivateLeftSideBarstyle"]');
-if(sideMenu.length > 0){
-    var lastElement = sideMenu[sideMenu.length -1];
-
-    matches.push(lastElement);
-
-    lastElement.remove();
+//find all livestream sections
+var livestreams = document.querySelectorAll("a[href*='/twitch']");
+for(var stream of livestreams){
+    stream.style.display = 'none';
 }
 
-//find the popoup video player
-var popup = document.querySelectorAll('[class*="VideoPopupstyle"]');
-for(var pop of popup){
-    matches.push(pop);
-    pop.remove();
-}
-
-//find the live-stream-section
-var streams = document.querySelectorAll('[class*="live-steam-section"]');
-for(var pop of streams){
-    matches.push(pop);
-    pop.remove();
-}
-
+//remove popup
 var iframes = document.querySelectorAll('iframe[src*="https://player.twitch.tv"]');
-for(var pop of iframes){
-    matches.push(pop);
-    pop.remove();
+for(var iframe of iframes){
+    iframe.style.display = 'none';
 }
 
-//find all iframes
-// var iframes = document.querySelectorAll('iframe');
-// for(var pop of iframes){
-//     matches.push(pop);
-//     pop.remove();
-// }
-
-console.log(matches);
+//find live streams text
+var text = 'live streams';
+for (var div of document.querySelectorAll('div')) {
+    if (div.textContent.toLowerCase() === text) {
+        div.style.display = 'none';
+    }
+}
